10b577ee87ea194aa2613e1ef94a62867532794d
Japa Alekhin Llemos <jap@alekhin.io>
2019-05-09 02:44:56 +0800

    added gameStart method added punch markers added game constants added fps display

--- lib/shadow-training.dart
+++ lib/shadow-training.dart
@@ -2,8 +2,10 @@ import 'dart:math';
 import 'dart:ui';
 import 'package:flame/game.dart';
 import 'package:flutter/gestures.dart';
+import 'package:flutter/painting.dart';
 import 'package:shadow_training/components/background.dart';
 import 'package:shadow_training/components/boxer.dart';
+import 'package:shadow_training/components/punch-marker.dart';
 import 'package:shadow_training/shadow-training-ui.dart';
 
 class ShadowTraining extends Game {
@@ -12,15 +14,58 @@ class ShadowTraining extends Game {
   Size screen;
   double screenScale;
   double screenHeight;
+  double fps;
+  TextPainter fpsT;
+  TextStyle fpsS;
+
+  // constants
+  final double minNextSpawn = .3;
+  final double maxNextSpawn = 3;
+  final double nextSpawnReductionFactor = .95;
+  final double maxSpeed = 10;
+  final double initialSpeed = 5;
+  final double speedIncrement = .05;
+
+  // game status
+  double gameSpeed = 0;
+  double nextSpawn = 0;
+  double runningSpawn = 0;
 
   // components
   Background background;
   Boxer boxer;
+  List<PunchMarker> markers;
 
   ShadowTraining(this.ui) {
+    fpsS = TextStyle(
+      fontSize: 25,
+      color: Color(0xffff0000),
+    );
+    fpsT = TextPainter(
+      textAlign: TextAlign.left,
+      textDirection: TextDirection.ltr,
+    );
+
     rnd = Random();
     background = Background(this);
     boxer = Boxer(this);
+    markers = List<PunchMarker>();
+  }
+
+  void start() {
+    gameSpeed = initialSpeed;
+    nextSpawn = maxNextSpawn;
+    runningSpawn = nextSpawn;
+    markers.clear();
+  }
+
+  void spawnMarker() {
+    PunchMarkerType type;
+    int r = rnd.nextInt(3);
+    if (r == 0) type = PunchMarkerType.left;
+    if (r == 1) type = PunchMarkerType.right;
+    if (r == 2) type = PunchMarkerType.up;
+    markers.add(PunchMarker(this, type));
   }
 
   void render(Canvas c) {
@@ -31,12 +76,36 @@ class ShadowTraining extends Game {
 
     background.render(c);
     boxer.render(c);
+    markers.forEach((PunchMarker m) => m.render(c));
 
     c.restore();
+
+    fpsT.paint(c, Offset.zero);
   }
 
   void update(double t) {
     boxer.update(t);
+    markers.forEach((PunchMarker m) => m.update(t));
+    markers.removeWhere((PunchMarker m) => m.isExpired);
+
+    if (ui.isTraining) {
+      runningSpawn -= t;
+      if (runningSpawn <= 0) {
+        nextSpawn = max(minNextSpawn, nextSpawn * nextSpawnReductionFactor);
+        runningSpawn = nextSpawn;
+        spawnMarker();
+      }
+      if (gameSpeed < maxSpeed) {
+        gameSpeed += speedIncrement * t;
+        if (gameSpeed > maxSpeed) {
+          gameSpeed = maxSpeed;
+        }
+      }
+    }
+
+    fps = 1 / t;
+    fpsT.text = TextSpan(text: fps.toString(), style: fpsS);
+    fpsT.layout();
   }
 
   void resize(Size s) {
