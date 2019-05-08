ad3173703f127124e6db1a48a33ccb77a37ba806
Japa Alekhin Llemos <jap@alekhin.io>
2019-05-08 14:19:55 +0800

    added awesome logic to fix resize problem added background component

--- lib/speed-training.dart
+++ lib/speed-training.dart
@@ -1,8 +1,31 @@
 import 'dart:ui';
+import 'package:basic_structure/components/background.dart';
 import 'package:flame/game.dart';
 
 class SpeedTraining extends Game {
-  void render(Canvas canvas) {}
+  Background background;
+  Size screen;
+  double screenHeight;
+
+  SpeedTraining() {
+    background = Background(this);
+  }
+
+  void render(Canvas c) {
+    if (screen == null) return;
+    c.save();
+    c.scale(screen.width / 9);
+    c.translate(0, screenHeight);
+
+    background.render(c);
+
+    c.restore();
+  }
 
   void update(double t) {}
+
+  void resize(Size s) {
+    screen = s;
+    screenHeight = screen.height / (screen.width / 9);
+  }
 }
